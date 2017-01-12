a2daaf9816077b3e01ebb47f8bd2e891d6b02fa2
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-01-12 11:36:09 +0400

    The creation was perfectly done

---
+++ PasswordGenerator/PasswordGenerator/Generator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace PasswordGenerator
+{
+    public class Generator
+    {
+        public static string PasswordGenerator(int length)
+        {
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                var data = new byte[16];
+                rng.GetBytes(data);
+                var seed = BitConverter.ToInt32(data, 0);
+                var rnd = new Random(seed);
+
+                string password = string.Empty;
+
+                for (int i = 0; i < length; i++)
+                {
+                    if (rnd.Next(1, 4) == 1)
+                        password += (char)rnd.Next(48, 57);
+                    else if (rnd.Next(1, 4) == 2)
+                        password += (char)rnd.Next(65, 90);
+                    else
+                        password += (char)rnd.Next(97, 122);
+                }
+                return password;
+            }
+        }
+    }
+}
---
+++ PasswordGenerator/UserProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using PasswordGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Generator.PasswordGenerator(10));
+        }
+    }
+}
